bbb298926b544b1587055ab5a734ebdbdeee4b4d
40053531 <40053531@live.napier.ac.uk>
2015-10-29 20:05:05 +0000

    end of night 1

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
+
+izanagi = ['Izanagi','Fool','1','Elec']
+pixie = ['pixie','Magician','2','Ice']
+saki_mitama = ['Saki Mitama','Priestess','11','Recovery']
+senri = ['Senri','Empress','9','Recovery']
+
+personas = {'Izanagi': izanagi,'Pixie':pixie,'Saki Mitama':saki_mitama,'Senri':senri}
+
 
 @app.route('/')
 def root():
-  return render_template('base.html'), 200
+  return render_template('home.html'), 200
 
-@app.route('/home/')
-def home():
-  return render_template('home.html'),200
+@app.route('/list/')
+def list():
+  return render_template('list.html'),200
+
+@app.route('/search/')
+def search():
+  if request.method == 'POST'
+    print request.form
+    q = request.form['q']
+    for  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
