b02c3619304b85f7c651c6b29ff0f21bb6bf06c8
40053531 <40053531@live.napier.ac.uk>
2015-10-30 14:09:20 +0000

    Persona Page base complete plus expands on information on personas

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ def personasAppend(bigList,key,list):
   bigList.append(dict(zip(key,list)))
   return None
 
-keys = ['name', 'arcana','starting level','inherits','img']
+keys = ['name',
+'arcana','startinglevel','inherits','reflects','absorbs','blocks','resists','weak','img']
 
-izanagi = ['Izanagi','Fool','1','Elec','img/Izanagi.jpg']
-pixie = ['Pixie','Magician','2','Ice','img/Pixie.jpg']
-saki_mitama =['Saki Mitama','Priestess','11','Recovery','img/Saki_Mitama.jpg']
-senri = ['Senri','Empress','9','Recovery','img/Senri.jpg']
-oberon = ['Oberon','Emperor','12','Elec','img/Oberon.jpg']
-omoikane = ['Omoikane','Hierophant','7','Elec','img/Omoikane.jpg']
-queen_mab = ['Queen Mab','Lovers','25','Elec','img/Queen_mab.jpg']
-slime = ['Slime','Chariot','2','Phys','img/Slime.jpg']
-angel = ['Angel','Justice','4','Wind','img/Angel.jpg']
+izanagi = ['Izanagi','Fool','1','Elec','-','-','Dark','Electric','Wind','img/Izanagi.jpg']
+pixie = ['Pixie','Magician','2','Ice','-','-','-','Wind','Fire','img/Pixie.jpg']
+saki_mitama =['Saki Mitama','Priestess','11','Recovery','-','-','-','Ice','Wind','img/Saki_Mitama.jpg']
+senri = ['Senri','Empress','9','Recovery','-','-','Fire','-','Electric','img/Senri.jpg']
+oberon = ['Oberon','Emperor','12','Elec','-','-','Electric','Fire','Wind','img/Oberon.jpg']
+omoikane = ['Omoikane','Hierophant','7','Elec','-','-','-','Electric','Ice','img/Omoikane.jpg']
+queen_mab = ['Queen Mab','Lovers','25','Electric','-','-','Electric','-','Wind','img/Queen_mab.jpg']
+slime = ['Slime','Chariot','2','Phys','-','-','-','Phys','Fire','img/Slime.jpg']
+angel = ['Angel','Justice','4','Wind','-','-','-','Wind,Light','Dark','img/Angel.jpg']
 
 
 personas = []
@@ -53,9 +54,10 @@ def search():
 @app.route('/persona/<chara>/')
 def persona(chara):
   for p in personas:
-    if p['name'] == chara
+    if p['name'] == chara:
       print p['name']
-  return render_template('persona.html')
+      return render_template('persona.html',p=p)
+  return "nope.jpg"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
