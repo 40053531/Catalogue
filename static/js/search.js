fab9f7de1b647a65f4719bd7c996dfe84802dc33
40053531 <40053531@live.napier.ac.uk>
2015-11-01 11:56:19 +0000

    removed duped directories

--- static/js/search.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $("#search_btn").click(function(){
-    var url = window.location.host + "/search/?q=" +term;
-     window.location.assign(url);
-     return true;
-  
-}}
